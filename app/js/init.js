e2e2bedf6920565814279ac607d4fa1660d16c59
Timurgorez <timuumit@mail.ru>
2017-03-12 11:47:53 +0200

    first

---
+++ app/js/init.js
@@ -0,0 +1,11 @@
+﻿//Управление и подключение фотослайдера на CodeIt.
+$(function(){
+	$('.slider1').mobilyslider({
+		transition: 'fade',
+		animationSpeed: 800,
+		autoplay: true,
+		autoplaySpeed: 7000,
+		bullets: true,
+		arrowsHide: false
+	});
+});
---
+++ dist/js/PopUp.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    PopUpHide();
+});
+function PopUpShow(click){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
